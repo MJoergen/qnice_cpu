d235231aa018fd0a4ce40108b3a7b14aa2502621
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2021-03-25 12:16:22 +0100

    Timing optimization in dp_ram

--- src/sub/dp_ram.vhd
+++ src/sub/dp_ram.vhd
@@ -2,22 +2,28 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std_unsigned.all;
 
+-- This is a single-clock dual-port RAM, with one write port and one read port.
+-- In case of Block RAMs an extra register is inserted, such that the memory is
+-- read on the falling clock. This improves timing.
+-- However, this trick does not work with distributed memory, but is not needed
+-- either.
+
 entity dp_ram is
    generic (
-      G_RAM_STYLE : string := "block";
+      G_RAM_STYLE : string := "block"; -- Select between "block" and "distributed".
       G_ADDR_SIZE : integer;
       G_DATA_SIZE : integer
    );
    port (
-      clk_i         : in  std_logic;
-      rst_i         : in  std_logic;
+      clk_i     : in  std_logic;
+      rst_i     : in  std_logic;
       -- Write interface
-      wr_addr_i     : in  std_logic_vector(G_ADDR_SIZE-1 downto 0);
-      wr_data_i     : in  std_logic_vector(G_DATA_SIZE-1 downto 0);
-      wr_en_i       : in  std_logic;
+      wr_addr_i : in  std_logic_vector(G_ADDR_SIZE-1 downto 0);
+      wr_data_i : in  std_logic_vector(G_DATA_SIZE-1 downto 0);
+      wr_en_i   : in  std_logic;
       -- Read interface
-      rd_addr_i     : in  std_logic_vector(G_ADDR_SIZE-1 downto 0);
-      rd_data_o     : out std_logic_vector(G_DATA_SIZE-1 downto 0)
+      rd_addr_i : in  std_logic_vector(G_ADDR_SIZE-1 downto 0);
+      rd_data_o : out std_logic_vector(G_DATA_SIZE-1 downto 0)
    );
 end entity dp_ram;
 
@@ -40,32 +46,40 @@ begin
          if wr_en_i = '1' then
             dp_ram_r(to_integer(wr_addr_i)) <= wr_data_i;
          end if;
-
--- pragma synthesis_off
-         if rst_i = '1' then
-            for i in 0 to 7 loop
-               dp_ram_r(i) <= X"111" * to_std_logic_vector(i, 4);
-            end loop;
-         end if;
--- pragma synthesis_on
       end if;
    end process p_write;
 
 
-   p_read_falling : process (clk_i)
-   begin
-      if falling_edge(clk_i) then
-         rd_data <= dp_ram_r(to_integer(rd_addr_i));
-      end if;
-   end process p_read_falling;
+   gen_block_ram : if G_RAM_STYLE = "block" generate
 
-   p_read_rising : process (clk_i)
-   begin
-      if rising_edge(clk_i) then
-         rd_data_o <= rd_data;
-      end if;
-   end process p_read_rising;
+      -- Block RAMs we read on the falling clock edge to improve timing.
 
+      p_read_falling : process (clk_i)
+      begin
+         if falling_edge(clk_i) then
+            rd_data <= dp_ram_r(to_integer(rd_addr_i));
+         end if;
+      end process p_read_falling;
+
+      p_read_rising : process (clk_i)
+      begin
+         if rising_edge(clk_i) then
+            rd_data_o <= rd_data;
+         end if;
+      end process p_read_rising;
+
+   else generate -- G_RAM_STYLE /= "block"
+
+      -- Distributed RAM we read on the rising clock edge, again to improve timing.
+
+      p_read : process (clk_i)
+      begin
+         if rising_edge(clk_i) then
+            rd_data_o <= dp_ram_r(to_integer(rd_addr_i));
+         end if;
+      end process p_read;
+
+   end generate gen_block_ram;
 
 end architecture synthesis;
 
