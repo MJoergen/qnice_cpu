ebf80c1b07533516afd2b9a10f2f242188770e10
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2021-04-18 14:35:03 +0200

    WIP

--- test/prog_pipeline.asm
+++ test/prog_pipeline.asm
@@ -1,15 +1,32 @@
       .ORG 0x0000
-      MOVE L_1, R0
+      MOVE L_1, R7
       ABRA L_START, 1
 
-      .ORG 0x0010
-L_START
-      ADD @R0++, @R0++
-      MOVE 0x1234, R8
-      MOVE 0x2345, R9
+      HALT
+      HALT
+      HALT
+      HALT
+      HALT
+      HALT
+      HALT
+      HALT
+      HALT
+      HALT
+      HALT
       HALT
 
-      .ORG 0x1000
+L_START
+      ADD @R7++, @R7++
+      MOVE 0x1234, R8
+      MOVE 0x2345, R9
+
+      HALT
+      HALT
+      HALT
+      HALT
+      HALT
+      HALT
+
 L_1   .DW 0x4321
 L_2   .DW 0x5432
 L_3   .DW 0x0000
