01299f2ee3d470ab3bf5b0dd47a061c3042a1ac8
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2021-04-26 22:29:30 +0200

    WIP

--- src/cpu_main/decode.vhd
+++ src/cpu_main/decode.vhd
@@ -74,10 +74,6 @@ architecture synthesis of decode is
    signal microcode_addr  : std_logic_vector(3 downto 0);
    signal microcode_value : std_logic_vector(35 downto 0);
 
-   signal seq_valid : std_logic;
-   signal seq_ready : std_logic;
-   signal seq_stage : t_stage;
-
 begin
 
    ------------------------------------------------------------
@@ -86,21 +82,21 @@ begin
 
    fetch_double_o <= immediate_src or immediate_dst;
    fetch_ready_o <= '0' when fetch_double_o and not fetch_double_i else -- Wait for immediate value
-                    seq_ready;
+                    prep_ready_i;
 
 
    ------------------------------------------------------------
    -- Generate combinatorial output values
    ------------------------------------------------------------
 
-   reg_rd_en_o    <= seq_ready; -- Read when next stage is ready to process data.
+   reg_rd_en_o    <= prep_ready_i; -- Read when next stage is ready to process data.
    reg_src_addr_o <= fetch_data_i(R_SRC_REG);
    reg_dst_addr_o <= to_stdlogicvector(C_REG_SP, 4) when fetch_data_i(R_OPCODE) = C_OPCODE_JMP else
                      fetch_data_i(R_DST_REG);
 
-   seq_stage.src_val <= reg_src_val_i; -- One clock cycle after reg_src_addr_o
-   seq_stage.dst_val <= reg_dst_val_i; -- One clock cycle after reg_dst_addr_o
-   seq_stage.r14     <= reg_r14_i;
+   prep_stage_o.src_val <= reg_src_val_i; -- One clock cycle after reg_src_addr_o
+   prep_stage_o.dst_val <= reg_dst_val_i; -- One clock cycle after reg_dst_addr_o
+   prep_stage_o.r14     <= reg_r14_i;
 
 
    ------------------------------------------------------------
@@ -122,10 +118,10 @@ begin
                     fetch_data_i(R_DST_MODE) = C_MODE_POST
                else '0';
 
-   reads_from_dst  <= C_READS_FROM_DST (to_integer(fetch_data_i(R_OPCODE)));
-   writes_to_dst   <= C_WRITES_TO_DST  (to_integer(fetch_data_i(R_OPCODE)));
-   src_memory      <= '0' when (fetch_data_i(R_SRC_MODE) = C_MODE_REG or immediate_src = '1') else has_src_operand;
-   dst_memory      <= '0' when (fetch_data_i(R_DST_MODE) = C_MODE_REG or immediate_dst = '1') else has_dst_operand;
+   reads_from_dst <= C_READS_FROM_DST (to_integer(fetch_data_i(R_OPCODE)));
+   writes_to_dst  <= C_WRITES_TO_DST  (to_integer(fetch_data_i(R_OPCODE)));
+   src_memory     <= '0' when (fetch_data_i(R_SRC_MODE) = C_MODE_REG or immediate_src = '1') else has_src_operand;
+   dst_memory     <= '0' when (fetch_data_i(R_DST_MODE) = C_MODE_REG or immediate_dst = '1') else has_dst_operand;
 
 
    ------------------------------------------------------------
@@ -152,36 +148,36 @@ begin
    begin
       if rising_edge(clk_i) then
          -- Next stage has consumed output data
-         if seq_ready = '1' then
-            seq_valid <= '0';
+         if prep_ready_i = '1' then
+            prep_valid_o <= '0';
          end if;
 
          -- Ready to send new data to next stage
          if fetch_valid_i and fetch_ready_o then
-            seq_valid <= '1';
-            seq_stage.microcodes <= microcode_value;
-            seq_stage.addr       <= fetch_addr_i;
-            seq_stage.immediate  <= fetch_data_i(R_IMMEDIATE);
-            seq_stage.inst       <= fetch_data_i(R_INSTRUCTION);
-            seq_stage.src_addr   <= reg_src_addr_o;
-            seq_stage.src_mode   <= fetch_data_i(R_SRC_MODE);
-            seq_stage.src_imm    <= immediate_src;
-            seq_stage.dst_addr   <= reg_dst_addr_o;
-            seq_stage.dst_mode   <= fetch_data_i(R_DST_MODE);
-            seq_stage.dst_imm    <= immediate_dst;
-            seq_stage.res_reg    <= reg_dst_addr_o;
+            prep_valid_o <= '1';
+            prep_stage_o.microcodes <= microcode_value;
+            prep_stage_o.addr       <= fetch_addr_i;
+            prep_stage_o.immediate  <= fetch_data_i(R_IMMEDIATE);
+            prep_stage_o.inst       <= fetch_data_i(R_INSTRUCTION);
+            prep_stage_o.src_addr   <= reg_src_addr_o;
+            prep_stage_o.src_mode   <= fetch_data_i(R_SRC_MODE);
+            prep_stage_o.src_imm    <= immediate_src;
+            prep_stage_o.dst_addr   <= reg_dst_addr_o;
+            prep_stage_o.dst_mode   <= fetch_data_i(R_DST_MODE);
+            prep_stage_o.dst_imm    <= immediate_dst;
+            prep_stage_o.res_reg    <= reg_dst_addr_o;
 
             -- Treat jumps as a special case
             if fetch_data_i(R_OPCODE) = C_OPCODE_JMP then
                -- Write new address to PC
-               seq_stage.res_reg <= to_stdlogicvector(C_REG_PC, 4);
+               prep_stage_o.res_reg <= to_stdlogicvector(C_REG_PC, 4);
                if src_memory = '0' then
-                  seq_stage.microcodes <= std_logic_vector'(
+                  prep_stage_o.microcodes <= std_logic_vector'(
                                       C_VAL_LAST &
                                       C_VAL_LAST &
                                       (C_VAL_LAST or C_VAL_REG_WRITE));
                else
-                  seq_stage.microcodes <= std_logic_vector'(
+                  prep_stage_o.microcodes <= std_logic_vector'(
                                       C_VAL_LAST &
                                       (C_VAL_LAST or C_VAL_MEM_WAIT_SRC or C_VAL_REG_WRITE) &
                                       (C_VAL_MEM_READ_SRC or C_VAL_REG_MOD_SRC));
@@ -190,15 +186,15 @@ begin
                -- Subroutine call
                if fetch_data_i(R_JMP_MODE) = C_JMP_ASUB or fetch_data_i(R_JMP_MODE) = C_JMP_RSUB then
                   -- Artifically introduce a MOVE R15, @--R13
-                  seq_stage.dst_addr <= to_stdlogicvector(C_REG_SP, 4);
-                  seq_stage.dst_mode <= to_stdlogicvector(C_MODE_PRE, 2);
+                  prep_stage_o.dst_addr <= to_stdlogicvector(C_REG_SP, 4);
+                  prep_stage_o.dst_mode <= to_stdlogicvector(C_MODE_PRE, 2);
                   if src_memory = '0' then
-                     seq_stage.microcodes <= std_logic_vector'(
+                     prep_stage_o.microcodes <= std_logic_vector'(
                                          C_VAL_LAST &
                                          (C_VAL_LAST or C_VAL_REG_WRITE) &
                                          (C_VAL_REG_MOD_DST or C_VAL_MEM_WRITE));
                   else
-                     seq_stage.microcodes <= std_logic_vector'(
+                     prep_stage_o.microcodes <= std_logic_vector'(
                                          (C_VAL_LAST or C_VAL_MEM_WAIT_SRC or C_VAL_REG_WRITE) &
                                          (C_VAL_MEM_READ_SRC or C_VAL_REG_MOD_SRC) &
                                          (C_VAL_REG_MOD_DST or C_VAL_MEM_WRITE));
@@ -208,33 +204,16 @@ begin
                -- Relative jump
                if immediate_src = '1' and
                   (fetch_data_i(R_JMP_MODE) = C_JMP_RBRA or fetch_data_i(R_JMP_MODE) = C_JMP_RSUB) then
-                  seq_stage.immediate <= fetch_data_i(R_IMMEDIATE) + fetch_addr_i + 2;
+                  prep_stage_o.immediate <= fetch_data_i(R_IMMEDIATE) + fetch_addr_i + 2;
                end if;
             end if;
          end if;
 
          if rst_i = '1' then
-            seq_valid <= '0';
+            prep_valid_o <= '0';
          end if;
       end if;
    end process p_output;
-
-
-   ------------------------------------------------------------
-   -- Sequencer
-   ------------------------------------------------------------
-
-   i_sequencer : entity work.sequencer
-      port map (
-         clk_i        => clk_i,
-         rst_i        => rst_i,
-         dec_valid_i  => seq_valid,
-         dec_ready_o  => seq_ready,
-         dec_stage_i  => seq_stage,
-         prep_valid_o => prep_valid_o,
-         prep_ready_i => prep_ready_i,
-         prep_stage_o => prep_stage_o
-      ); -- i_sequencer
 
 end architecture synthesis;
 
--- src/cpu_main/prepare.vhd
+++ src/cpu_main/prepare.vhd
@@ -31,6 +31,10 @@ end entity prepare;
 
 architecture synthesis of prepare is
 
+   signal seq_valid : std_logic;
+   signal seq_ready : std_logic;
+   signal seq_stage : t_stage;
+
    signal wait_for_mem_req : std_logic;
    signal wait_for_mem_src : std_logic;
    signal wait_for_mem_dst : std_logic;
@@ -44,37 +48,54 @@ architecture synthesis of prepare is
 begin
 
    ------------------------------------------------------------
+   -- Sequencer
+   ------------------------------------------------------------
+
+   i_sequencer : entity work.sequencer
+      port map (
+         clk_i        => clk_i,
+         rst_i        => rst_i,
+         dec_valid_i  => dec_valid_i,
+         dec_ready_o  => dec_ready_o,
+         dec_stage_i  => dec_stage_i,
+         prep_valid_o => seq_valid,
+         prep_ready_i => seq_ready,
+         prep_stage_o => seq_stage
+      ); -- i_sequencer
+
+
+   ------------------------------------------------------------
    -- Get values read from memory
    ------------------------------------------------------------
 
-   wait_for_mem_req <= dec_valid_i and or(dec_stage_i.microcodes(2 downto 0)) and not wr_ready_i;
-   wait_for_mem_src <= dec_valid_i and dec_stage_i.microcodes(C_MEM_WAIT_SRC) and not mem_src_valid_i;
-   wait_for_mem_dst <= dec_valid_i and dec_stage_i.microcodes(C_MEM_WAIT_DST) and not mem_dst_valid_i;
+   wait_for_mem_req <= seq_valid and or(seq_stage.microcodes(2 downto 0)) and not wr_ready_i;
+   wait_for_mem_src <= seq_valid and seq_stage.microcodes(C_MEM_WAIT_SRC) and not mem_src_valid_i;
+   wait_for_mem_dst <= seq_valid and seq_stage.microcodes(C_MEM_WAIT_DST) and not mem_dst_valid_i;
 
 
    ------------------------------------------------------------
    -- Back-pressure
    ------------------------------------------------------------
 
-   mem_src_ready_o <= dec_valid_i and dec_stage_i.microcodes(C_MEM_WAIT_SRC) and not wait_for_mem_dst;
-   mem_dst_ready_o <= dec_valid_i and dec_stage_i.microcodes(C_MEM_WAIT_DST) and not wait_for_mem_src;
-   dec_ready_o <= not (wait_for_mem_req or wait_for_mem_src or wait_for_mem_dst);
+   mem_src_ready_o <= seq_valid and seq_stage.microcodes(C_MEM_WAIT_SRC) and not wait_for_mem_dst;
+   mem_dst_ready_o <= seq_valid and seq_stage.microcodes(C_MEM_WAIT_DST) and not wait_for_mem_src;
+   seq_ready <= wr_ready_i and not (wait_for_mem_req or wait_for_mem_src or wait_for_mem_dst);
 
 
    ------------------------------------------------------------
    -- ALU
    ------------------------------------------------------------
 
-   alu_oper    <= dec_stage_i.inst(R_OPCODE);
-   alu_ctrl    <= dec_stage_i.inst(R_CTRL_CMD);
-   alu_flags   <= dec_stage_i.r14;
-   alu_src_val <= dec_stage_i.immediate when dec_stage_i.src_imm else
-                  mem_src_data_i when dec_stage_i.microcodes(C_MEM_WAIT_SRC) = '1' else
-                  dec_stage_i.addr+1 when dec_stage_i.src_addr = C_REG_PC else
-                  dec_stage_i.src_val;
-   alu_dst_val <= dec_stage_i.immediate when dec_stage_i.dst_imm else
-                  mem_dst_data_i when dec_stage_i.microcodes(C_MEM_WAIT_DST) = '1' else
-                  dec_stage_i.dst_val;
+   alu_oper    <= seq_stage.inst(R_OPCODE);
+   alu_ctrl    <= seq_stage.inst(R_CTRL_CMD);
+   alu_flags   <= seq_stage.r14;
+   alu_src_val <= seq_stage.immediate when seq_stage.src_imm else
+                  mem_src_data_i when seq_stage.microcodes(C_MEM_WAIT_SRC) = '1' else
+                  seq_stage.addr+1 when seq_stage.src_addr = C_REG_PC else
+                  seq_stage.src_val;
+   alu_dst_val <= seq_stage.immediate when seq_stage.dst_imm else
+                  mem_dst_data_i when seq_stage.microcodes(C_MEM_WAIT_DST) = '1' else
+                  seq_stage.dst_val;
 
 
    ------------------------------------------------------------
@@ -90,9 +111,9 @@ begin
          end if;
 
          -- Ready to send new data to next stage
-         if dec_valid_i and dec_ready_o then
-            wr_valid_o             <= dec_valid_i;
-            wr_stage_o             <= dec_stage_i;
+         if seq_valid and seq_ready then
+            wr_valid_o             <= seq_valid;
+            wr_stage_o             <= seq_stage;
             wr_stage_o.alu_oper    <= alu_oper;
             wr_stage_o.alu_ctrl    <= alu_ctrl;
             wr_stage_o.alu_flags   <= alu_flags;
